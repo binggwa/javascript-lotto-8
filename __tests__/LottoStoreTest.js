fcc4edf91e4887b93093605362d031266b1a234e
binggwa <qudrhks317@gmail.com>
2025-10-30 18:01:55 +0900

    test(LottoStoreTest.js) : buyLotto(purchasePrice) 테스트 추가

--- __tests__/LottoStoreTest.js
+++ __tests__/LottoStoreTest.js
@@ -21,4 +21,13 @@ describe('LottoStore 동작 테스트', () => {
     const lottos = LottoStore.pickLottoNumbersAndSort();
     expect(lottos.getNumbers()).toEqual(input);
   });
+
+  test('buyLotto는 구매 가격/1000만큼 로또 구매', () => {
+    mockRandoms([
+      [1, 2, 3, 4, 5, 6],
+      [40, 41, 42, 43, 44, 45],
+    ]);
+    const lottos = LottoStore.buyLotto(2000);
+    expect(lottos).toHaveLength(2);
+  });
 });
