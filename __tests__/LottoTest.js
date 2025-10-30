53d904728e0c2db633f090a4400844e6ea2a1699
binggwa <qudrhks317@gmail.com>
2025-10-30 15:26:22 +0900

    test(LottoTest.js) : Lotto 클래스에 대한 테스트 케이스 추가
    - 로또 번호 개수 검증 테스트 추가
    - 로또 번호 중복 테스트 추가
    - 로또 번호 범위 1~45 검증 테스트 추가
    - 로또 번호 정상 생성 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,32 @@
 import Lotto from "../src/Lotto";
 
 describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+  test.each([
+    { nums: [1, 2, 3, 4, 5], message: '로또 번호의 개수가 6개보다 부족하면 예외가 발생한다.'},
+    { nums: [1, 2, 3, 4, 5, 6, 7], message: '로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.'},
+  ])('로또 번호 개수 검증 테스트: $message', ({ nums }) => {
+    expect(() => new Lotto(nums)).toThrow(/^\[ERROR\]/);
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+  test.each([
+    { nums: [1, 2, 3, 4, 5, 5] },
+    { nums: [10, 11, 12, 13, 13, 13] },
+    { nums: [45, 44, 43, 43, 43, 43] },
+  ])('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', ({ nums }) => {
+    expect(() => new Lotto(nums)).toThrow(/^\[ERROR\]/);
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test.each([
+    { nums: [0, 1, 2, 3, 4, 5], message: '하한 edge 케이스 확인(0)' },
+    { nums: [46, 45, 44, 43, 42, 41], message: '상한 edge 케이스 확인(45)' },
+  ])('로또 번호가 1~45 범위를 가지는지 검증한다: $message', ({ nums }) => {
+    expect(() => new Lotto(nums)).toThrow(/^\[ERROR\]/);
+  });
+
+  test.each([
+    { nums: [1, 2, 3, 4, 5, 6] },
+    { nums: [45, 44, 43, 42, 41, 40] },
+  ])('로또 번호 정상 생성 테스트', ({ nums }) => {
+    expect(() => new Lotto(nums)).not.toThrow();
+  });
 });
