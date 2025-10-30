7963eac695ade1cddcd441f8e8ba79a564dc9918
binggwa <qudrhks317@gmail.com>
2025-10-30 17:14:06 +0900

    refactor(App.js) : UserInput.js로 기능이관
    - 당첨번호 입력 및 검증 기능 이관
    - 보너스번호 입력 및 검증 기능 이관

--- src/App.js
+++ src/App.js
@@ -34,26 +34,28 @@ class App {
         MissionUtils.Console.print(`[${lottoNumbers.join(', ')}]`);
       }
       
-      // 당첨번호 입력받아서 저장
-      const winningNumbersStr = await MissionUtils.Console.readLineAsync('\n당첨 번호를 입력해 주세요\n');
-      const winningNumbers = winningNumbersStr.split(',').map(numStr => Number(numStr.trim())).filter(num => !Number.isNaN(num));
+      // // 당첨번호 입력받아서 저장
+      // const winningNumbersStr = await MissionUtils.Console.readLineAsync('\n당첨 번호를 입력해 주세요\n');
+      // const winningNumbers = winningNumbersStr.split(',').map(numStr => Number(numStr.trim())).filter(num => !Number.isNaN(num));
 
-      // 당첨번호 검증
-      if (winningNumbers.length !== 6) throw new Error('[ERROR] 당첨 번호는 6개여야 합니다!');
-      if (winningNumbers < 1 || winningNumbers > 45) throw new Error('[ERROR] 당첨번호는 1~45 범위여야 합니다!');
-      if (new Set(winningNumbers).size !== 6) throw new Error('[ERROR] 당첨 번호는 중복되지 않아야 합니다!');
+      // // 당첨번호 검증
+      // if (winningNumbers.length !== 6) throw new Error('[ERROR] 당첨 번호는 6개여야 합니다!');
+      // if (winningNumbers < 1 || winningNumbers > 45) throw new Error('[ERROR] 당첨번호는 1~45 범위여야 합니다!');
+      // if (new Set(winningNumbers).size !== 6) throw new Error('[ERROR] 당첨 번호는 중복되지 않아야 합니다!');
+      const winningNumbers = await UserInput.readWinningNumbers();
       
-      // 보너스번호 입력받아서 저장 
-      const bonusNumberStr = await MissionUtils.Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n');
-      const bonusNumber = Number(bonusNumberStr.trim());
+      // // 보너스번호 입력받아서 저장 
+      // const bonusNumberStr = await MissionUtils.Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n');
+      // const bonusNumber = Number(bonusNumberStr.trim());
 
-      // 보너스번호 검증
-      if (Number.isNaN(bonusNumber) || bonusNumber < 1 || bonusNumber > 45) {
-        throw new Error('[ERROR] 보너스 번호는 1~45 범위의 숫자여야 합니다!');
-      }
-      if (winningNumbers.includes(bonusNumber)) {
-        throw new Error('[ERROR] 보너스 번호가 당첨번호와 중복됩니다!');
-      }
+      // // 보너스번호 검증
+      // if (Number.isNaN(bonusNumber) || bonusNumber < 1 || bonusNumber > 45) {
+      //   throw new Error('[ERROR] 보너스 번호는 1~45 범위의 숫자여야 합니다!');
+      // }
+      // if (winningNumbers.includes(bonusNumber)) {
+      //   throw new Error('[ERROR] 보너스 번호가 당첨번호와 중복됩니다!');
+      // }
+      const bonusNumber = await UserInput.readBonusNumber(winningNumbers);
 
       // 당첨번호와 로또번호가 몇 개 맞는지 확인
       let [fifth, fourth, third, second, first] = [0, 0, 0, 0, 0];
