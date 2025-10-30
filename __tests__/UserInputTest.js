a79921986873a1473e4f74fe1f0b28a079a13072
binggwa <qudrhks317@gmail.com>
2025-10-30 17:12:17 +0900

    test(UserInputTest.js) : 보너스 번호 검증테스트 추가
    - 보너스 번호 정상 테스트
    - 보너스 번호 예외 테스트
      - 숫자 아님
      - 하한 이탈
      - 상한 이탈
      - 당첨 번호와 중복

--- __tests__/UserInputTest.js
+++ __tests__/UserInputTest.js
@@ -55,4 +55,22 @@ describe('UserInput 검증 테스트', () => {
     await expect(UserInput.readWinningNumbers()).rejects.toThrow(/^\[ERROR\]/);
   });
 
+  test.each([
+    { input: '1', winNumber: [2, 3, 4, 5, 6, 7], expected: 1 },
+    { input: '45', winNumber: [1, 2, 3, 4, 5, 6], expected: 45 },
+  ])('보너스 번호 정상 테스트: $input', async ({ input, winNumber, expected }) => {
+    mockQuestions([input]);
+    await expect(UserInput.readBonusNumber(winNumber)).resolves.toBe(expected);
+  });
+
+  test.each([
+    { input: 'a', winNumber: [1, 2, 3, 4, 5, 6], message: '보너스 번호 숫자 아님' },
+    { input: '0', winNumber: [1, 2, 3, 4, 5, 6], message: '보너스 번호 하한 이탈' },
+    { input: '46', winNumber: [1, 2, 3, 4, 5, 6], message: '보너스 번호 상한 이탈' },
+    { input: '1', winNumber: [1, 2, 3, 4, 5, 6], message: '당첨 번호와 중복' },
+  ])('보너스 번호 예외 테스트: "$input", $message', async ({ input, winNumber }) => {
+    mockQuestions([input]);
+    await expect(UserInput.readBonusNumber(winNumber)).rejects.toThrow(/^\[ERROR\]/);
+  });
+
 });
