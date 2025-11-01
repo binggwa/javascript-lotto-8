0b1ea0471bbb24382a378c39cc102f9b72914162
binggwa <qudrhks317@gmail.com>
2025-11-01 20:31:35 +0900

    refactor(App.js) : run()메소드 리팩토링으로 메소드 분리
    - #purchaseLottos()
      - 구입금액 입력 및 검증, 로또 구매를 한 메소드로 통합, 간결화
    - #printPurchased(purchasePrice, lottos)
      - 구매한 로또개수와 로또번호 출력 통합
    - #drawLottoNumbers()
      - 당첨번호 및 보너스번호 입력 및 검증 통합
    - #printStatistics(rankCounts, purchasePrice)
      - 당첨 통계 출력 및 총 수익률 출력 통합

--- src/App.js
+++ src/App.js
@@ -7,27 +7,49 @@ import LottoResult from './LottoResult.js';
 class App {
   async run() {
     try {
+      const { purchasePrice, lottos } = await this.#purchaseLottos();
+      this.#printPurchased(purchasePrice, lottos);
+
+      const { winningNumbers, bonusNumber } = await this.#drawLottoNumbers();
+
+      // 당첨번호와 로또번호가 몇 개 맞는지 확인
+      const rankCounts = LottoResult.match(lottos, winningNumbers, bonusNumber);
+      this.#printStatistics(rankCounts, purchasePrice);
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      throw error;
+    }
+  } 
+
+  async #purchaseLottos() {
       // 구입금액 입력받아서 저장 및 검증
       const purchasePrice = await UserInput.readPurchasePrice();
 
       // 로또 구매
       const lottos = LottoStore.buyLotto(purchasePrice);
-      
+
+      return { purchasePrice, lottos };
+  }
+
+  #printPurchased(purchasePrice, lottos) {
       // 구매한 로또 갯수 출력
       OutputFormat.printLottoTicketsAmount(purchasePrice);
 
       // 구매한 로또번호 출력
       OutputFormat.printLottoNumbers(lottos);
-      
-      // 당첨번호 입력받아서 저장 및 검증
-      const winningNumbers = await UserInput.readWinningNumbers();
-      
-      // 보너스번호 입력받아서 저장 및 검증
-      const bonusNumber = await UserInput.readBonusNumber(winningNumbers);
+  }
 
-      // 당첨번호와 로또번호가 몇 개 맞는지 확인
-      const rankCounts = LottoResult.match(lottos, winningNumbers, bonusNumber);
+  #drawLottoNumbers() {
+    // 당첨번호 입력받아서 저장 및 검증
+    const winningNumbers = await UserInput.readWinningNumbers();
+      
+    // 보너스번호 입력받아서 저장 및 검증
+    const bonusNumber = await UserInput.readBonusNumber(winningNumbers);
 
+    return { winningNumbers, bonusNumber };
+  }
+
+  #printStatistics(rankCounts, purchasePrice) {
       // 당첨 통계 출력
       OutputFormat.printStatistics(rankCounts);
 
@@ -39,11 +61,7 @@ class App {
 
       // 총 수익률 출력
       OutputFormat.printTotalReturn(totalReturn);
-    } catch (error) {
-      MissionUtils.Console.print(error.message);
-      throw error;
-    }
-  } 
+  }
 }
 
 export default App;
