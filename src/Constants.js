e6ceecf44bbc980c621613a17e67cdfa69c8d75b
binggwa <qudrhks317@gmail.com>
2025-11-02 19:08:50 +0900

    refactor(Constants) : 상수 클래스 단순화
    - 클래스에서 상수 각각을 export하는 형식으로 단순화
    - 상금을 묶어 간단화
    - PRICE_UNIT 이름 변경 -> LOTTO_PRICE

--- src/Constants.js
+++ src/Constants.js
@@ -1,18 +1,11 @@
-class Constants {
-  // 로또 장당 가격
-  static PRICE_UNIT = 1_000;
-
-  // 로또 번호 범위, 갯수
-  static MIN_NUMBER_RANGE = 1;
-  static MAX_NUMBER_RANGE = 45;
-  static LOTTO_NUMBERS_PER_TICKET = 6;
-
-  // 랭크별 상금
-  static FIRST_PRIZE = 2_000_000_000;
-  static SECOND_PRIZE = 30_000_000;
-  static THIRE_PRIZE = 1_500_000;
-  static FOURTH_PRIZE = 50_000;
-  static FIFTH_PRIZE = 5_000;
-}
-
-export default Constants;
+export const LOTTO_PRICE = 1_000;
+export const LOTTO_NUMBERS_PER_TICKET = 6;
+export const MIN_NUMBER_RANGE = 1;
+export const MAX_NUMBER_RANGE = 45;
+export const PRIZES = Object.freeze([
+  2_000_000_000, 
+  30_000_000, 
+  1_500_000, 
+  50_000, 
+  5_000,
+]);
