4365e9895ed43bb941cf66a039df71e31a95613b
binggwa <qudrhks317@gmail.com>
2025-11-02 19:47:02 +0900

    refactor(LottoCompany) : LottoResult 기능을 LottoCompany로 이관
    - match() -> getMatchedCounts()로 이름 변경
      - LottoCompany 안에 winningNumbers와 bonusNumber 변수를 넣어 파라미터
        변경
    - getRank() 메소드
      - 우승상금 상수를 PRIZE로 통합함으로써 코드 간결화
    - calculateTotalPrize() -> getTotalPrize()로 이름 변경
    - calculateTotalReturn()은 구매자가 계산하는 것으로 LottoBuyer 새
      클래스 작성 후 이관 예정

--- src/LottoCompany.js
+++ src/LottoCompany.js
@@ -2,7 +2,8 @@ import {
   LOTTO_NUMBERS_PER_TICKET, 
   LOTTO_PRICE, 
   MAX_NUMBER_RANGE, 
-  MIN_NUMBER_RANGE 
+  MIN_NUMBER_RANGE, 
+  PRIZES
 } from './Constants';
 
 class LottoCompany {
@@ -28,5 +29,47 @@ class LottoCompany {
     ).sort((a, b) => a - b);
 
     return new Lotto(lottoNumbers);
+  }
+
+  getMatchedCounts(lottos) {
+    // 0: 1등, 1: 2등, 2: 3등, 3: 4등, 4: 5등
+    const rankCounts = Array.from({ length: 5 }, () => 0);
+
+    lottos.forEach((lotto) => {
+      const lottoNumbers = lotto.getNumbers();
+      const matchCount = this.#getMatchCount(lottoNumbers);
+      const rank = this.#getRank(matchCount, lottoNumbers);
+      if (rank) {
+        rankCounts[rank - 1]++;
+      }
+    });
+
+    return rankCounts;
+  }
+
+  #getMatchCount(lottoNumbers) {
+    return lottoNumbers.filter((num) => this.#winningNumbers.includes(num)).length;
+  }
+
+  #getRank(matchCount, lottoNumbers) {
+    if (matchCount === 6) {
+      return 1;
+    } else if (matchCount === 5) {
+      // 보너스 번호의 일치여부에 따른 2등과 3등 구분
+      const hasBonus = lottoNumbers.includes(this.#bonusNumber);
+      return 2 + !hasBonus;
+    } else if (matchCount === 4) {
+      return 4;
+    } else if (matchCount === 3) {
+      return 5;
+    }
+
+    return null;
+  }
+
+  getTotalPrize(rankCounts) {
+    return rankCounts.reduce((total, count, index) => {
+      return total + count * PRIZES[index];
+    }, 0);
   }
 }
