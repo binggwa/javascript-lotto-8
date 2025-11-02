2f5ab156a1c98e4a675db2a1e5026886c2d28739
binggwa <qudrhks317@gmail.com>
2025-11-02 21:44:53 +0900

    refactor(ConsoleOutput) : OutputFormat -> ConsoleOutput으로 이름변경
    - 상수 변경으로 직접 import
    - rankCounts 변경으로 간결화

---
+++ src/ConsoleOutput.js
@@ -0,0 +1,31 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { LOTTO_PRICE } from './Constants';
+
+class ConsoleOutput {
+  static printLottoTicketsAmount(purchasePrice){
+    const lottoTicketsAmount = (purchasePrice / LOTTO_PRICE);
+    MissionUtils.Console.print(`\n${lottoTicketsAmount}개를 구매했습니다.`);
+  }
+
+  static printLottoNumbers(lottos) {
+    lottos.forEach((lotto) => {
+      const nums = lotto.getNumbers();
+      MissionUtils.Console.print(`[${nums.join(', ')}]`);
+    });
+  }
+
+  static printStatistics(rankCounts) {
+    MissionUtils.Console.print('\n당첨 통계\n---');
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${rankCounts[4]}개`);
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${rankCounts[3]}개`);
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${rankCounts[2]}개`);
+    MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${rankCounts[1]}개`);
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${rankCounts[0]}개`);
+  }
+
+  static printTotalReturn(totalReturn) {
+    MissionUtils.Console.print(`총 수익률은 ${totalReturn}%입니다.`);
+  }
+}
+
+export default ConsoleOutput;
