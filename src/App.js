695b2210fb63a27c11286e56115c01e83b9c40e2
binggwa <qudrhks317@gmail.com>
2025-11-02 23:59:14 +0900

    refactor(App) : LottoBuyer, LottoCompany 클래스 생성으로 인한 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,67 +1,87 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
-import UserInput from './UserInput.js';
-import OutputFormat from './OutputFormat.js';
-import LottoStore from './LottoStore.js';
-import LottoResult from './LottoResult.js';
+import ConsoleInput from './ConsoleInput.js';
+import ConsoleOutput from './ConsoleOutput.js';
+import LottoBuyer from './LottoBuyer.js';
+import LottoCompany from './LottoCompany.js';
 
 class App {
   async run() {
-    try {
-      const { purchasePrice, lottos } = await this.#purchaseLottos();
-      this.#printPurchased(purchasePrice, lottos);
+    // 구입할 로또 금액 입력받기
+    const purchasePrice = await ConsoleInput.readPurchasePrice();
 
-      const { winningNumbers, bonusNumber } = await this.#drawLottoNumbers();
+    // 로또 구매
+    const lottoBuyer = new LottoBuyer(purchasePrice);
+    const lottoCompany = new LottoCompany();
+    lottoBuyer.buyLottos(lottoCompany);
 
-      // 당첨번호와 로또번호가 몇 개 맞는지 확인
-      const rankCounts = LottoResult.match(lottos, winningNumbers, bonusNumber);
-      this.#printStatistics(rankCounts, purchasePrice);
-    } catch (error) {
-      MissionUtils.Console.print(error.message);
-      throw error;
-    }
-  } 
+    // 구매내역 출력
+    ConsoleOutput.printLottoTicketsAmount(purchasePrice);
+    ConsoleOutput.printLottoNumbers(lottoBuyer.getLottos());
 
-  async #purchaseLottos() {
-      // 구입금액 입력받아서 저장 및 검증
-      const purchasePrice = await UserInput.readPurchasePrice();
+    // 당첨번호, 보너스 번호 입력
+    const winningNumbers = await ConsoleInput.readWinningNumbers();
+    const bonusNumber = await ConsoleInput.readBonusNumber(winningNumbers);
+    lottoCompany.setWinningNumbers(winningNumbers);
+    lottoCompany.setBonusNumber(bonusNumber);
 
-      // 로또 구매
-      const lottos = LottoStore.buyLotto(purchasePrice);
+    // 당첨 결과 집계 및 출력
+    const rankCounts = lottoCompany.getMatchedCounts(lottoBuyer.getLottos());
+    ConsoleOutput.printStatistics(rankCounts);
 
-      return { purchasePrice, lottos };
+    // 총 당첨금 계산 및 출력
+    const totalPrize = lottoCompany.getTotalPrize(rankCounts);
+    const totalProfitRate = lottoBuyer.getProfitRate(totalPrize);
+    ConsoleOutput.printTotalReturn(totalProfitRate);
+      // const { purchasePrice, lottos } = await this.#purchaseLottos();
+      // this.#printPurchased(purchasePrice, lottos);
+
+      // const { winningNumbers, bonusNumber } = await this.#drawLottoNumbers();
+
+      // // 당첨번호와 로또번호가 몇 개 맞는지 확인
+      // const rankCounts = LottoResult.match(lottos, winningNumbers, bonusNumber);
+      // this.#printStatistics(rankCounts, purchasePrice);
   }
+} 
 
-  #printPurchased(purchasePrice, lottos) {
-      // 구매한 로또 갯수 출력
-      OutputFormat.printLottoTicketsAmount(purchasePrice);
+  // async #purchaseLottos() {
+  //     // 구입금액 입력받아서 저장 및 검증
+  //     const purchasePrice = await UserInput.readPurchasePrice();
 
-      // 구매한 로또번호 출력
-      OutputFormat.printLottoNumbers(lottos);
-  }
+  //     // 로또 구매
+  //     const lottos = LottoStore.issueLottos(purchasePrice);
 
-  async #drawLottoNumbers() {
-    // 당첨번호 입력받아서 저장 및 검증
-    const winningNumbers = await UserInput.readWinningNumbers();
+  //     return { purchasePrice, lottos };
+  // }
+
+  // #printPurchased(purchasePrice, lottos) {
+  //     // 구매한 로또 갯수 출력
+  //     OutputFormat.printLottoTicketsAmount(purchasePrice);
+
+  //     // 구매한 로또번호 출력
+  //     OutputFormat.printLottoNumbers(lottos);
+  // }
+
+  // async #drawLottoNumbers() {
+  //   // 당첨번호 입력받아서 저장 및 검증
+  //   const winningNumbers = await UserInput.readWinningNumbers();
       
-    // 보너스번호 입력받아서 저장 및 검증
-    const bonusNumber = await UserInput.readBonusNumber(winningNumbers);
+  //   // 보너스번호 입력받아서 저장 및 검증
+  //   const bonusNumber = await UserInput.readBonusNumber(winningNumbers);
 
-    return { winningNumbers, bonusNumber };
-  }
+  //   return { winningNumbers, bonusNumber };
+  // }
 
-  #printStatistics(rankCounts, purchasePrice) {
-      // 당첨 통계 출력
-      OutputFormat.printStatistics(rankCounts);
+  // #printStatistics(rankCounts, purchasePrice) {
+  //     // 당첨 통계 출력
+  //     OutputFormat.printStatistics(rankCounts);
 
-      // 총 당첨상금 계산
-      const totalPrize = LottoResult.calculateTotalPrize(rankCounts);
+  //     // 총 당첨상금 계산
+  //     const totalPrize = LottoResult.calculateTotalPrize(rankCounts);
 
-      // 총 수익률 계산
-      const totalReturn = LottoResult.calculateTotalReturn(totalPrize, purchasePrice);
+  //     // 총 수익률 계산
+  //     const totalReturn = LottoResult.calculateTotalReturn(totalPrize, purchasePrice);
 
-      // 총 수익률 출력
-      OutputFormat.printTotalReturn(totalReturn);
-  }
-}
+  //     // 총 수익률 출력
+  //     OutputFormat.printTotalReturn(totalReturn);
+  // }
 
 export default App;
