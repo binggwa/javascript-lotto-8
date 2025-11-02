d4054521276d8d6a63494c097aa4ccbd9d46fe3d
binggwa <qudrhks317@gmail.com>
2025-11-02 21:41:05 +0900

    refactor(ConsoleInput) : UserInput -> ConsoleInput으로 이름변경
    - 상수 클래스 삭제 후 직접 export로 상수 간결화

--- src/ConsoleInput.js
+++ src/ConsoleInput.js
@@ -1,7 +1,12 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import Constants from './Constants';
+import { 
+  LOTTO_NUMBERS_PER_TICKET,
+  LOTTO_PRICE, 
+  MAX_NUMBER_RANGE, 
+  MIN_NUMBER_RANGE 
+} from './Constants';
 
-class UserInput {
+class ConsoleInput {
   static async readPurchasePrice() {
     const purchasePriceStr = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
 
@@ -20,18 +25,21 @@ class UserInput {
     if (Number.isNaN(price)) {
       throw new Error('[ERROR] 구입 금액이 숫자가 아닙니다!');
     }
-    if (price < Constants.PRICE_UNIT) {
-      throw new Error(`[ERROR] 구입 최소 금액은 ${Constants.PRICE_UNIT}원 입니다!`);
+    if (price < LOTTO_PRICE) {
+      throw new Error(`[ERROR] 구입 최소 금액은 ${LOTTO_PRICE}원 입니다!`);
     }
-    if (price % Constants.PRICE_UNIT !== 0) {
-      throw new Error(`[ERROR] 구입 금액은 ${Constants.PRICE_UNIT}의 배수여야 합니다!`);
+    if (price % LOTTO_PRICE !== 0) {
+      throw new Error(`[ERROR] 구입 금액은 ${LOTTO_PRICE}의 배수여야 합니다!`);
     }
   }
 
   static async readWinningNumbers() {
     // 당첨번호 입력받아서 저장
     const winningNumbersStr = await MissionUtils.Console.readLineAsync('\n당첨 번호를 입력해 주세요\n');
-    const winningNumbers = winningNumbersStr.split(',').map(numStr => Number(numStr.trim())).filter(num => !Number.isNaN(num));
+    const winningNumbers = winningNumbersStr
+      .split(',')
+      .map(numStr => Number(numStr.trim()))
+      .filter(num => !Number.isNaN(num));
 
     // 당첨번호 검증
     this.#validateWinningNumbers(winningNumbers);
@@ -40,16 +48,18 @@ class UserInput {
   }
 
   static #validateWinningNumbers(nums) {
-    if (nums.length !== Constants.LOTTO_NUMBERS_PER_TICKET) {
-      throw new Error(`[ERROR] 당첨 번호는 ${Constants.LOTTO_NUMBERS_PER_TICKET}개여야 합니다!`);
+    if (nums.length !== LOTTO_NUMBERS_PER_TICKET) {
+      throw new Error(`[ERROR] 당첨 번호는 ${LOTTO_NUMBERS_PER_TICKET}개여야 합니다!`);
     }
 
-    const isOutRange = nums.some((num) => num < Constants.MIN_NUMBER_RANGE || num > Constants.MAX_NUMBER_RANGE);
+    const isOutRange = nums.some((num) => num < MIN_NUMBER_RANGE || num > MAX_NUMBER_RANGE);
     if (isOutRange) {
-      throw new Error(`[ERROR] 당첨 번호는 ${Constants.MIN_NUMBER_RANGE}~${Constants.MAX_NUMBER_RANGE} 범위여야 합니다!`);
+      throw new Error(`[ERROR] 당첨 번호는 ${MIN_NUMBER_RANGE}~${MAX_NUMBER_RANGE} 범위여야 합니다!`);
     }
     
-    if (new Set(nums).size !== Constants.LOTTO_NUMBERS_PER_TICKET) throw new Error('[ERROR] 당첨 번호는 중복되지 않아야 합니다!');
+    if (new Set(nums).size !== LOTTO_NUMBERS_PER_TICKET) {
+      throw new Error('[ERROR] 당첨 번호는 중복되지 않아야 합니다!');
+    }
   }
 
   static async readBonusNumber(winningNumbers) {
@@ -64,8 +74,8 @@ class UserInput {
   }
 
   static #validateBonusNumber(bonus, winningNumbers) {
-    if (Number.isNaN(bonus) || bonus < Constants.MIN_NUMBER_RANGE || bonus > Constants.MAX_NUMBER_RANGE) {
-      throw new Error(`[ERROR] 보너스 번호는 ${Constants.MIN_NUMBER_RANGE}~${Constants.MAX_NUMBER_RANGE} 범위의 숫자여야 합니다!`);
+    if (Number.isNaN(bonus) || bonus < MIN_NUMBER_RANGE || bonus > MAX_NUMBER_RANGE) {
+      throw new Error(`[ERROR] 보너스 번호는 ${MIN_NUMBER_RANGE}~${MAX_NUMBER_RANGE} 범위의 숫자여야 합니다!`);
     }
     if (winningNumbers.includes(bonus)) {
       throw new Error('[ERROR] 보너스 번호가 당첨번호와 중복됩니다!');
